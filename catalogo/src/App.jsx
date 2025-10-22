60f8b8f7efed6f3a2867903af8eff3d075642838
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2025-10-22 12:49:58 +0200

    creamos la estructura de ficheros y comenzamos con el indice inicial

---
+++ catalogo/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido a mi catálogo</h1>
+      <p>Aquí podrás explorar nuestros productos.</p>
+    </div>
+  )
+}
+
+export default App
